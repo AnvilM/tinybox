58374dcd8078fddd3aacdea1cdfb8b223660333b
anvilm <211anvil112@gmail.com>
2026-02-25 19:24:50 +0300

    feat(SingBoxConfig): handle runtime exceptions when cannot find paths

--- src/Infrastructure/Shared/Config/SingBoxConfig.php
+++ src/Infrastructure/Shared/Config/SingBoxConfig.php
@@ -4,29 +4,48 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Shared\Config;
 
+use App\Core\Shared\Exception\CriticalException;
 use App\Core\Shared\Ports\Config\SingBox\SingBoxConfigPort;
 use Application\Config\SingBoxConfig\SingBoxConfig as Config;
+use RuntimeException;
 
 final readonly class SingBoxConfig implements SingBoxConfigPort
 {
     public static function singBoxOutboundTemplatePath(): string
     {
-        return Config::singBoxOutboundTemplatePath();
+        try {
+            return Config::singBoxOutboundTemplatePath();
+        } catch (RuntimeException $exception) {
+            throw new CriticalException("Unable to find outbound template path", $exception->getMessage());
+        }
     }
 
     public static function singBoxUrltestOutboundTemplatePath(): string
     {
-        return Config::singBoxUrltestOutboundTemplatePath();
+        try {
+            return Config::singBoxUrltestOutboundTemplatePath();
+        } catch (RuntimeException $exception) {
+            throw new CriticalException("Unable to find urltest outbound template path", $exception->getMessage());
+        }
+
     }
 
     public static function singBoxConfigTemplatePath(): string
     {
-        return Config::singBoxConfigTemplatePath();
+        try {
+            return Config::singBoxConfigTemplatePath();
+        } catch (RuntimeException $exception) {
+            throw new CriticalException("Unable to find sing-box config template path", $exception->getMessage());
+        }
     }
 
     public static function singBoxConfigSaveDirectoryPath(): string
     {
-        return Config::singBoxConfigSaveDirectoryPath();
+        try {
+            return Config::singBoxConfigSaveDirectoryPath();
+        } catch (RuntimeException $exception) {
+            throw new CriticalException("Unable to find sing-box config save directory path", $exception->getMessage());
+        }
     }
 
 }
