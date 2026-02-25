f97533a2bb0b1cfe8846cbabd58a48273face066
anvilm <211anvil112@gmail.com>
2026-02-25 19:43:50 +0300

    feat(ApplySubscriptions): add ApplySubscription

---
+++ src/Application/ApplySubscriptionConfig/Handler/ApplySubscriptionConfigHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\ApplySubscriptionConfig\Handler;
+
+use App\Application\ApplySubscriptionConfig\Command\ApplySubscriptionConfigCommand;
+
+final readonly class ApplySubscriptionConfigHandler
+{
+    public function handle(ApplySubscriptionConfigCommand $command): void
+    {
+
+    }
+}
