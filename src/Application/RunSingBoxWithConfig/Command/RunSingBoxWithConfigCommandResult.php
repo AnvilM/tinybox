c4649272afd40a1e634e3976efe4a8e317d12ce5
anvilm <211anvil112@gmail.com>
2026-03-01 05:25:59 +0300

    feat: Run sing box binary

--- src/Application/RunSingBoxWithConfig/Command/RunSingBoxWithConfigCommandResult.php
+++ src/Application/RunSingBoxWithConfig/Command/RunSingBoxWithConfigCommandResult.php
@@ -6,5 +6,9 @@ namespace App\Application\RunSingBoxWithConfig\Command;
 
 final readonly class RunSingBoxWithConfigCommandResult
 {
-
+    public function __construct(
+        public int $responseCode,
+    )
+    {
+    }
 }
--- src/Application/RunSingBoxWithConfig/Process/RunSingBox.php
+++ src/Application/RunSingBoxWithConfig/Process/RunSingBox.php
@@ -4,7 +4,43 @@ declare(strict_types=1);
 
 namespace App\Application\RunSingBoxWithConfig\Process;
 
+use App\Core\Shared\Exception\CriticalException;
+use App\Core\Shared\Ports\Config\ConfigFactoryPort;
+
 final readonly class RunSingBox
 {
+    public function __construct(
+        private ConfigFactoryPort $configFactoryPort,
+    )
+    {
+    }
 
+    public function run(string $subscriptionName): int
+    {
+        echo "running $subscriptionName\n";
+
+        $descriptors = [
+            0 => STDIN,
+            1 => STDOUT,
+            2 => STDERR
+        ];
+
+        $command = "sudo "
+            . $this->configFactoryPort->get()->singBoxConfig->binary
+            . " run -c "
+            . $this->configFactoryPort->get()->generatedConfigsDirectoryPath
+            . "/$subscriptionName.json";
+
+        $singBoxProcess = proc_open(
+            $command,
+            $descriptors,
+            $pipes
+        );
+
+        if (is_resource($singBoxProcess)) {
+            return proc_close($singBoxProcess);
+        }
+
+        throw new CriticalException("Unable to run sing-box");
+    }
 }
