2fb50940a4716de8d61d6dc54bac7ac814b264c3
anvilm <211anvil112@gmail.com>
2026-03-02 05:31:37 +0300

    feat: add configs:list command

---
+++ src/Application/ScanGeneratedConfigsDirectory/Handler/ScanGeneratedConfigsDirectoryHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\ScanGeneratedConfigsDirectory\Handler;
+
+use App\Application\ScanGeneratedConfigsDirectory\Command\ScanGeneratedConfigsDirectoryCommandResult;
+use App\Core\Shared\Exception\CriticalException;
+use App\Core\Shared\Ports\Config\ConfigFactoryPort;
+use App\Core\Shared\Ports\IO\Directory\ScanDirectoryForFilesPort;
+use InvalidArgumentException;
+use RuntimeException;
+
+final readonly class ScanGeneratedConfigsDirectoryHandler
+{
+    public function __construct(
+        private ScanDirectoryForFilesPort $scanDirectoryForFilesPort,
+        private ConfigFactoryPort         $configFactoryPort,
+    )
+    {
+    }
+
+    /**
+     * Scan generated configs directory and return filenames from directory
+     *
+     * @return ScanGeneratedConfigsDirectoryCommandResult
+     *
+     * @throws CriticalException Throws if unable to read generated configs directory or is not a valid directory
+     */
+    public function handle(): ScanGeneratedConfigsDirectoryCommandResult
+    {
+        /**
+         * Get generated config directory path from app config
+         */
+        $generatedConfigsDirectoryPath = $this->configFactoryPort->get()->generatedConfigsDirectoryPath;
+
+        try {
+            /**
+             * Scan directory for files
+             */
+            $configFiles = $this->scanDirectoryForFilesPort->scan($generatedConfigsDirectoryPath);
+
+        } catch (InvalidArgumentException|RuntimeException $e) {
+
+            /**
+             * Throw critical exception on unable to scan directory
+             */
+            throw new CriticalException(($e instanceof InvalidArgumentException)
+                ? "Not a valid directory provided"
+                : "Unable to read directory",
+                $generatedConfigsDirectoryPath
+            );
+        }
+
+        $result = [];
+
+        foreach ($configFiles as $configFile) {
+            $extension = pathinfo($configFile, PATHINFO_EXTENSION);
+
+            if ($extension === '') {
+                $result[] = $configFile;
+                continue;
+            }
+
+            $result[] = substr($configFile, 0, -(strlen($extension) + 1));
+        }
+
+        return new ScanGeneratedConfigsDirectoryCommandResult(
+            $result
+        );
+    }
+}
