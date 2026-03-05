94641ff627363ff792f3be9dc27d06b055beb866
anvilm <211anvil112@gmail.com>
2026-03-05 03:04:50 +0300

    refactor(os): remove AbstractOSHelper and OS.php, restructure directory helpers

---
+++ src/Infrastructure/Shared/OS/Directories/GetConfigsDirectory.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Shared\OS\Directories;
+
+use App\Core\Shared\Exception\CriticalException;
+use App\Core\Shared\Ports\OS\Directories\GetConfigsDirectoryPort;
+use Application\Config\ApplicationConfig\ApplicationConfig;
+use RuntimeException;
+use UnexpectedValueException;
+
+final readonly class GetConfigsDirectory implements GetConfigsDirectoryPort
+{
+    public function execute(): string
+    {
+        return match (PHP_OS_FAMILY) {
+            'Linux' => $this->executeLinux(),
+            default => throw new CriticalException("Unsupported OS: " . PHP_OS_FAMILY)
+        };
+    }
+
+    private function executeLinux(): string
+    {
+        if (getenv('HOME') === false) {
+            throw new RuntimeException("Cannot get HOME environment variable");
+        }
+
+
+        if (!is_string(getenv('HOME'))) {
+            throw new UnexpectedValueException("HOME environment variable must be a string");
+        }
+
+
+        return getenv('HOME') . '/.config/' . ApplicationConfig::appName . '/';
+    }
+}
