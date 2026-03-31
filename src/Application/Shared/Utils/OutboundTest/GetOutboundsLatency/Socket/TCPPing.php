d76ca992a9417005da7332767d280524e5903f03
anvilm <211anvil112@gmail.com>
2026-03-31 23:07:02 +0300

    fix: tcp ping connection

--- src/Application/Shared/Utils/OutboundTest/GetOutboundsLatency/Socket/TCPPing.php
+++ src/Application/Shared/Utils/OutboundTest/GetOutboundsLatency/Socket/TCPPing.php
@@ -9,6 +9,7 @@ use App\Domain\Outbound\Collection\OutboundMap;
 use App\Domain\Shared\Exception\CriticalException;
 use Exception;
 use Psl\Async\Exception\CompositeException;
+use Psl\Async\TimeoutCancellationToken;
 use Psl\DateTime\Duration;
 use function Psl\Async\concurrently;
 use function Psl\Async\run;
@@ -43,7 +44,7 @@ final readonly class TCPPing
                     $port = $outbound->getServerPort();
                     if ($port === null || $server === null) throw new Exception();
 
-                    $socket = connect($server, $port, false, Duration::seconds(10));
+                    $socket = connect($server, $port, cancellation: new TimeoutCancellationToken(Duration::seconds(10)));
                     $socket->close();
 
                 })->catch(fn() => $result->setFailed())->await();
